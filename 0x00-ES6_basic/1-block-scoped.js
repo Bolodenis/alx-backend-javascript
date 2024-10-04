72bb443aaac424506ef173e25fd6050bf3ae609b
Bolodenis <bollodenis@gmail.com>
2024-10-04 13:28:31 +0000

    task2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,9 @@
 export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
-
   if (trueOrFalse) {
-    let task = true;  // This task is scoped to the if block
-    let task2 = false; // This task2 is scoped to the if block
+    let task = true;
+    let task2 = false;
   }
-
-  return [task, task2]; // Returns the outer task and task2
+  return [task, task2];
 }
