22dd758ad637d619c097b90ee802002a441ce03d
Bolodenis <bollodenis@gmail.com>
2024-10-06 17:44:38 +0000

    es6

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,9 @@
-import ClassRoom from './0-classroom';
+import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  return [19, 20, 34].map((room) => new ClassRoom(room));
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
